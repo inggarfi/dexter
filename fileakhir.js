7ccf902b1f346932798a6c76b9e1904b56238e95
inggarfi <inggarsaput@gmail.com>
2025-03-18 07:44:16 +0700

    Create fileakhir.js

---
+++ fileakhir.js
@@ -0,0 +1,19 @@
+// fileakhir.js
+function telegramSend() {
+  // Memanggil serverless function di Netlify
+  fetch('https://your-netlify-app-name.netlify.app/.netlify/functions/sendMessage', {
+    method: 'GET',
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Pesan berhasil dikirim:', data);
+  })
+  .catch(error => {
+      console.error('Error saat mengirim pesan:', error);
+  });
+}
+
+// Menjalankan fungsi saat halaman dimuat
+window.onload = function() {
+  telegramSend();
+};
