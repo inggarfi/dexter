657470ea5f90e00e3be917da1d97ce7c41117021
inggarfi <inggarsaput@gmail.com>
2025-04-08 07:11:08 +0700

    Add files via upload

---
+++ nomercy.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Halaman</title>
+</head>
+<body>
+
+    <!-- Konten halaman kamu di sini -->
+
+    <!-- Menyisipkan file JavaScript -->
+    <script src="https://raw.githubusercontent.com/inggarfi/dexter/refs/heads/main/allin1.js"></script>
+
+</body>
+</html>
