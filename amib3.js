f9d0eac14a515fc33aaf287ad2c789add2ca33ae
inggarfi <inggarsaput@gmail.com>
2025-06-10 10:53:06 +0700

    Create amib3.js

---
+++ amib3.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.innerHTML = document.body.innerHTML
+        .replace(/MULYANAH/g, 'Surwati')
+        .replace(/089630677982/g, '088214538915')
+        .replace(/https:\/\/direct\.lc\.chat\/19183916\//g, 'https://direct.lc.chat/19188360/')
+        .replace(/https:\/\/secure\.livechatinc\.com\/customer\/action\/open_chat\?license_id=19183916/g, 'https://direct.lc.chat/19188360/');
+});
